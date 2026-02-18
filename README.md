9a15b11bfd052a2f0bc638ef3d3071d88a296295
Muhira7799 <muhiramohammed99@gmail.com>
2026-02-18 00:52:51 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-12th-project
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Muhira7799/my-12th-project)
---
+++ index.ts
@@ -0,0 +1,20 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+// Create an array of fruits
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// Print the whole array
+console.log(fruits);
+
+// Print a specific item (index starts from 0)
+console.log("First fruit: " + fruits[0]);
+
+// Add a new fruit
+fruits.push("Grapes");
+
+// Loop through the array
+for (let i = 0; i < fruits.length; i++) {
+    console.log("Fruit: " + fruits[i]);
+}
